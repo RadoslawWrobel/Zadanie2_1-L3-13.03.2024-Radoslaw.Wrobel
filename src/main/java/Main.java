eb27583f5a19ee22be09bb65030dffae62f0eedb
Radoslaw Wrobel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-03-13 13:49:51 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.InputMismatchException;
+
+public class Main
+  {
+  public static void main(String[] args)
+    {
+    {
+      {
+        System.out.println("Hello World!");
+        String HelloWorld = "Hello World!";
+        for (int i = 0; i < HelloWorld.length(); i++)
+          {
+            System.out.println(HelloWorld.charAt(i));
+          }
+      }
+    }
+  }
+}
